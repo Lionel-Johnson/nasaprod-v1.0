bd151c07c73eca79d20f978aef13f0444910a235
Lionel-Johnson <104918212+Lionel-Johnson@users.noreply.github.com>
2023-05-22 05:50:47 +0100

    nasa g8 est un systhème de gestion des données personnalisées des militants du PDG développé en PHP, js, Mysql et css

---
+++ Common/footer.php
@@ -0,0 +1,44 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Nasa G8</h3>
+            <p>This Project has been made by Lionel-Brice NGOUVI MOUKENDOU with the help of 
+              my Allah Almighty.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Liens Rapides</h2>
+            <ul>
+              <li><a href="">About ICBS</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: NASA -G8,
+                  Port-Gentil – Gabon</p>
+              <p>Telephones: (+241) 65391828</p>
+              <p>E-mail: contact@nasapdg.org</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by Lionel-Brice NGOUVI MOUKENDOU - LMS GABON.2023</p>
+</footer>
